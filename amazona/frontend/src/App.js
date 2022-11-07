7daca9c080b1c9792d59ab89b596c09d8d960a51
Phú Đạt <phongdat.20022000@gmail.com>
2022-11-07 17:57:32 +0700

    create-react-app

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        <h3>list products</h3>
+      </main>
+    </div>
+  );
+};
+
+export default App;
