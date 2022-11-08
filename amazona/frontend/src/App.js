a2d2ae84eb3bbc8edd616cd59eba1eacbd35ef8a
Phú Đạt <phongdat.20022000@gmail.com>
2022-11-09 06:05:20 +0700

    Add-routing

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "./data";
 
 const App = () => {
   return (
@@ -8,6 +9,24 @@ const App = () => {
       </header>
       <main>
         <h3>list products</h3>
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="product" key={product.id}>
+                <a href={`/product/${product.slug}}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                <a href={`/product/${product.slug}}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>{product.price}</p>
+                  <button className="bnt-cart">Add to Cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
